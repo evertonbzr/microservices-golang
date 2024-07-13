d8382d177d8a2e12d194a548362d23395e5e1006
evertonbzr <evertonfrnds@gmail.com>
2024-07-13 15:32:48 -0300

    add database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"log/slog"
+	"os"
+
+	"github.com/evertonbzr/microservices-golang/internal/config"
+	"github.com/evertonbzr/microservices-golang/internal/db"
+)
+
+func main() {
+	config.Load(os.Getenv("ENV"))
+
+	slog.Info("Starting API...", "port", config.PORT, "env", config.ENV)
+
+	db := db.InitDB(config.POSTGRES_URL)
+	slog.Info("Database connected", "url", config.POSTGRES_URL)
+
+}
---
+++ internal/db/db.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"log"
+
+	"github.com/evertonbzr/microservices-golang/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB(uri string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(uri), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database", "error", err)
+	}
+
+	if config.IsDevelopment() {
+		db = db.Debug()
+	}
+
+	return db
+}
